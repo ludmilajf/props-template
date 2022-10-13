dc0854859e56a12ebbb06401efe06c8309ec3b25
ludmilajf <111311576+ludmilajf@users.noreply.github.com>
2022-10-13 12:54:23 -0300

    Exercícios concluídos (#1)

--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,13 +1,40 @@
 import Carro from "./Carro";
 
-function Garagem() {
+function Garagem(props) {
+  console.log(props)
   return (
     <div>
-      <h1>Garagem de Indio</h1>
-      <Carro />
-      <Carro />
-      <Carro />
-      <Carro />
+      <h1>Garagem de {props.nome}</h1>
+      <button onClick={props.mensagemApresentacao}>Mensagem</button>
+
+      <Carro 
+      nome={"Mobi"}
+      cor={"Vermelho"}
+      ano={2022}
+      flex={"True"}
+      adicionadoPor={props.nome}
+      />
+      <Carro 
+      nome={"Civic"}
+      cor={"Prata"}
+      ano={2019}
+      flex={"False"}
+      adicionadoPor={props.nome}
+      />
+      <Carro 
+      nome={"Ka"}
+      cor={"Preto"}
+      ano={2020}
+      flex={"False"}
+      adicionadoPor={props.nome}
+      />
+      <Carro 
+      nome={"Kwid"}
+      cor={"Branco"}
+      ano={2022}
+      flex={"True"}
+      adicionadoPor={props.nome}
+      />
     </div>
   );
 }
