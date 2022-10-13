be04649d94fb9720966ca872bf1678467717aa47
ludmilajf <ludmiladjf@gmail.com>
2022-10-13 12:53:26 -0300

    Exercícios concluídos

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import Garagem from "./Componentes/Garagem";
 import "./styles.css";
 
 export default function App() {
+  const nome1 = "Ludmila"
+  const nome2 = "Tiago"
+  function apresentaGaragem1(){
+    alert(`Boas vindas à garagem de ${nome1}!`)
+  }
+  function apresentaGaragem2(){
+    alert(`Boas vindas à garagem de ${nome2}!`)
+  }
   return (
     <div className="App">
-      <Garagem />
+      <Garagem nome={nome1}
+      mensagemApresentacao = {apresentaGaragem1}/>
+
+      <Garagem nome={nome2} mensagemApresentacao = {apresentaGaragem2}/>
+
     </div>
   );
 }
